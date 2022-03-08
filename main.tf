9e8abc36d6f038fbf5f88497b8f2b391d8ea17a0
nikcbg <nikcbg@yahoo.com>
2022-03-08 09:11:56 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 variable "computer_name" {
-  type    = "string"
+ # type    = "string"
   default = "1"
 }
 
 resource "random_pet" "computers" {
-  length = "${var.computer_name}"
+  length = var.computer_name
 }
 
 output "name" {
-  value = "${random_pet.computers.id}"
+  value = random_pet.computers.id
 }
       
